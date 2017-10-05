4874bb830f80f1dc18b0fb6824b4d298b2682278
jt024 <31736978+jt024@users.noreply.github.com>
2017-10-05 17:04:39 -0500

    Final Version

---
+++ MyLibrary/src/Toolkit.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trevi
+ */
+public class Toolkit {
+ 
+    
+    
+    public static class Sort{
+
+        public static int bubble(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int insertion(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int shell(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int selection(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int quick(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int merge(int[] arr)[] {
+        
+            return arr;
+        };
+        public static int heap(int[] arr)[] {
+        
+            return arr;
+        };    
+        
+        
+    }
+
+    
+}
